df3c9cf570fdabbfa191a4b0742082318a0eff3a
Daniel Kenney <danielkenney99@yahoo.com>
2023-01-18 19:22:15 -0800

    React native styling

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Here are some boxes of different colours
+        </Text>
+        <Text
+          style={[
+            styles.boxes,
+            { backgroundColor: "#2aa198", borderRadius: 3 },
+          ]}
+        >
+          Cyan #2aa198
+        </Text>
+        <Text style={[styles.boxes, { backgroundColor: "#268bd2" }]}>
+          Blue #268bd2
+        </Text>
+        <Text style={[styles.boxes, { backgroundColor: "#d33682" }]}>
+          Magenta #d33682
+        </Text>
+        <Text style={[styles.boxes, { backgroundColor: "#cb4b16" }]}>
+          Orange #cb4b16
+        </Text>
+      </View>
+    </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    marginTop: 50,
+    marginLeft: 20,
+    marginRight: 20,
+  },
+  title: {
+    alignSelf: "flex-start",
+    fontWeight: "bold",
+  },
+  boxes: {
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+    padding: 10,
+    color: "white",
+    margin: 5,
+    width: "100%",
   },
 });
+
+export default App;
